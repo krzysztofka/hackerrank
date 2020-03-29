f589e879301dd03678fe0ecbc7b4a3193abed396
kalisiakk <kalisiakk@gmail.com>
2020-03-29 21:18:11 +0200

    FreqQuery

--- src/main/java/com/kali/algo/MinimumSwaps.java
+++ src/main/java/com/kali/algo/MinimumSwaps.java
@@ -1,23 +1,9 @@
 package com.kali.algo;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 /**
  * You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates.
  * You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
  * <p>
- * For example, given the array [7,1,3,2,4,5,6] we perform 5 steps:
- * 1 -> 7
- * 2 -> 1
- * 3 -> 3
- * 4 -> 2
- * 5 -> 4
- * 6 -> 5
- * 7 -> 6
  */
 public class MinimumSwaps {
 
